d23e39cf7d87c974079ab06cecbcd8cc1f846fe5
Samira Ahmed <B00156786@mytudublin.ie>
2024-10-15 15:29:37 +0100

    part 2 completed

--- createMemberDropdown.php
+++ createMemberDropdown.php
@@ -16,15 +16,15 @@ if (!$conn) {
 $sql = "SELECT * from member";
 $result = mysqli_query($conn, $sql);
 
-echo "<TABLE border='1'>";
+echo "<select name='memberID'>";
 
 while($row = mysqli_fetch_assoc($result)) {
     $id=$row['id'];
     $fn=$row['firstname'];
     $sn=$row['surname'];
-    echo "<TR><TD>$id</TD><TD>$fn</TD><TD>$sn</TD></TR>";
+    echo "<option value='$id'>$fn $sn</option>";
 }
-echo "</TABLE>";
+echo "</select>";
 
 mysqli_close($conn); 
 ?>
